f2d33591c6d5244527e9601408e3a838121315a1
LuizMatt97 <luiz.fsouzacontato@gmail.com>
2022-11-07 17:01:54 -0300

    versao 1.1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 function carregar(){
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
+    let footer = document.querySelector('footer')
+    let titulo = document.querySelector('header')
+    let sec = document.querySelector('section')
     var data = new Date()
     var hora = data.getHours()
 
     msg.innerHTML = `Agora são ${hora} horas.`
     if( hora >= 0 && hora < 12){
         img.src = 'img/fotodia.png'
+        sec.style.background = 'orange'
         document.body.style.background = '#F0E68C'
             // Bom dia!
     } else if ( hora >= 12 && hora <= 18){
         img.src = 'img/fototarde.png'
+        sec.style.background = '#008080'
         document.body.style.background = '#B0E0E6'
           // Boa tarde!
     } else{
         img.src = 'img/fotonoite.png'
         document.body.style.background = '#1C1C1C'
+        sec.style.background = 'red'
+        footer.style.color = 'white'
+        titulo.style.color = 'white'
          // Boa noite!
     }
 }
